3db3a5bbed1ea51c311a8ba8a1500da300c4ad6e
moorissa tjokro <moorissa.maura@gmail.com>
2025-06-02 21:29:16 -0700

    Remove build directories from tracking

--- src/quiz/cluster/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_quizCluster_FILE /Users/moorissa/Repos/sensor_fusion/lidar-obstacle-detector/src/quiz/cluster/build/quizCluster)
-set(__QT_DEPLOY_TARGET_quizCluster_TYPE EXECUTABLE)
